e8f4cbbda58018ad8e60c7eee5166170a630d429
Matthieu Audemard <33572957+taridev@users.noreply.github.com>
2018-09-27 22:15:37 +0200

    Gestion des scores et création écran game over

--- pyshoot.py
+++ pyshoot.py
@@ -9,7 +9,9 @@ ship_folder = os.path.join(img_folder, 'ship')
 meteor_folder = os.path.join(img_folder, 'meteor')
 laser_folder = os.path.join(img_folder, 'laser')
 numeral_folder = os.path.join(img_folder, 'numeral')
-explosion_folder = os.path.join(img_folder, 'explosion')
+explosion_folder = os.path.join(img_folder, 'effect')
+powerup_folder = os.path.join(img_folder, 'powerup')
+effect_folder = os.path.join(img_folder, 'effect')
 
 WIDTH = 600
 HEIGHT = 800
@@ -47,7 +49,6 @@ class Player(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
         self.image = player_imgs['default']
-        self.image.set_colorkey(BLACK)  # Permet de rendre le noir transparent
         self.rect = self.image.get_rect()
         self.radius = int(self.rect.width * .80 / 2)
         self.rect.centerx = WIDTH / 2
@@ -56,12 +57,12 @@ class Player(pg.sprite.Sprite):
         self.speedy = 0
         self.shoot_delay = 250
         self.last_shot = pg.time.get_ticks()
+        self.gun_type = 1
 
     def update(self, *args):
         self.speedx = 0
         self.speedy = 0
         self.image = player_imgs['default']
-        self.image.set_colorkey(BLACK)  # Permet de rendre le noir transparent
         keystate = pg.key.get_pressed()
         # Gestion des tirs
         if keystate[pg.K_SPACE]:
@@ -93,11 +94,22 @@ class Player(pg.sprite.Sprite):
             shoot = Laser(self.rect.centerx, self.rect.top)
             all_sprites.add(shoot)
             lasers.add(shoot)
+            if self.gun_type > 1:
+                shoot1 = Laser(self.rect.left, self.rect.top + 30)
+                all_sprites.add(shoot1)
+                lasers.add(shoot1)
+                shoot2 = Laser(self.rect.right, self.rect.top + 30)
+                all_sprites.add(shoot2)
+                lasers.add(shoot2)
+
+    def power_up(self):
+        if self.gun_type < 3:
+            self.gun_type += 1
 
 
 class Meteor(pg.sprite.Sprite):
 
-    def __init__(self, size=None):
+    def __init__(self, size=None, center=None, speed=None):
         pg.sprite.Sprite.__init__(self)
         if size is None:
             size = random.choice(['sm', 'sm', 'sm', 'md', 'md', 'lg'])
@@ -106,10 +118,10 @@ class Meteor(pg.sprite.Sprite):
         self.image_orig = self.image.copy()
         self.rect = self.image.get_rect()
         self.radius = int(self.rect.width * .80 / 2)
-        self.rect.x = random.randrange(WIDTH - self.rect.width)
-        self.rect.y = random.randrange(-100, -40)
-        self.speedy = random.randrange(1, 8)
-        self.speedx = random.randrange(-3, 3)
+        self.rect.x = random.randrange(WIDTH - self.rect.width) if center is None else center[0]
+        self.rect.y = random.randrange(-100, -40) if center is None else center[1]
+        self.speedx = random.randrange(-3, 3) if speed is None else speed[0]
+        self.speedy = random.randrange(1, 8) if speed is None else speed[1]
         self.rot = 0
         self.rot_speed = random.randrange(-8, 8)
         self.last_update = pg.time.get_ticks()
@@ -129,17 +141,21 @@ class Meteor(pg.sprite.Sprite):
         self.rotate()
         self.rect.y += self.speedy
         self.rect.x += self.speedx
-        if self.rect.top > HEIGHT + 2 * self.radius or self.rect.left < -2 * self.radius or self.rect.right > WIDTH + 25:
+        # Si un meteore sors du cadre on le repositionne et on lui change sa vitesse et sa taille
+        if self.rect.top > HEIGHT + 2 * self.radius \
+                or self.rect.left < -2 * self.radius \
+                or self.rect.right > WIDTH + 25:
             self.rect.x = random.randrange(WIDTH - self.rect.width)
             self.rect.y = random.randrange(-100, -40)
             self.speedy = random.randrange(1, 8)
             self.speedx = random.randrange(-3, 3)
+            self.size = random.choice(['sm', 'sm', 'sm', 'md', 'md', 'lg'])
 
 
 class Laser(pg.sprite.Sprite):
     def __init__(self, x, y):
         pg.sprite.Sprite.__init__(self)
-        self.image = laser_img_list[2]
+        self.image = laser_img_list[1]
         self.image.set_colorkey(BLACK)  # Permet de rendre le noir transparent
         self.rect = self.image.get_rect()
         self.rect = self.image.get_rect()
@@ -184,20 +200,89 @@ class Ufo(pg.sprite.Sprite):
         pg.sprite.Sprite.__init__(self)
 
 
-def new_meteor():
-    meteor = Meteor()
+class Pow(pg.sprite.Sprite):
+    def __init__(self, center):
+        pg.sprite.Sprite.__init__(self)
+        self.type = random.choice(list(power_imgs.keys()))
+        self.image = power_imgs[self.type]
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.speedy = 5
+
+    def update(self, *args):
+        self.rect.y += self.speedy
+        # On détruit le powerup s'il dépasse l'écran
+        if self.rect.top > HEIGHT:
+            self.kill()
+
+
+class ScoreNumeral(pg.sprite.Sprite):
+    def __init__(self, numeral_string, x, y):
+        pg.sprite.Sprite.__init__(self)
+        if int(numeral_string) in range(9):
+            self.image = numeral_img[int(numeral_string)]
+        else:
+            self.image = numeral_img[0]
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+
+
+def new_meteor(size=None, center=None, speed=None):
+    meteor = Meteor(size, center, speed)
     mobs.add(meteor)
     all_sprites.add(meteor)
 
 
+def show_gameover():
+    waiting = True
+    while waiting:
+        clock.tick(FPS)
+        screen.blit(background_img, background_rect)
+        # Gestion des messages a afficher
+        draw_text(screen, "py SHOOT!", 64, WIDTH / 2, HEIGHT / 4)
+        draw_text(screen, "Arrow keys to move, Space to fire", 22, WIDTH / 2, HEIGHT / 2)
+        draw_text(screen, "Press Return to start", 18, WIDTH / 2, HEIGHT * 3 / 4)
+        # Gestion du curseur
+        cursor_rect = cursor_img.get_rect()
+        cursor_rect.center = pg.mouse.get_pos()
+        screen.blit(cursor_img, cursor_rect)
+        pg.display.flip()
+        for e in pg.event.get():
+            if e.type == pg.QUIT or e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE:
+                waiting = False
+                pg.event.post(pg.event.Event(pg.QUIT))
+            if e.type == pg.KEYUP and e.key == pg.K_RETURN:
+                waiting = False
+
+
+def draw_score(score_to_draw):
+    score_string = str(score_to_draw)
+    score_string = ('0' * (len(score_sprites) - len(score_string))) + score_string
+    score_len = len(score_sprites) - 1
+
+    for score_char in score_string[::-1]:
+        score_sprites[score_len].image = numeral_img[int(score_char)]
+        score_len -= 1
+
+
 # Graphics
-background = pg.image.load(os.path.join(img_folder, 'texture.png')).convert()
-background_rect = background.get_rect()
+background_img = pg.image.load(os.path.join(img_folder, 'texture.png')).convert()
+background_rect = background_img.get_rect()
 # Chargement des images du joueur
-player_imgs = {'default': pg.image.load(os.path.join(ship_folder, 'player.png')).convert(),
-               'left': pg.image.load(os.path.join(ship_folder, 'playerLeft.png')).convert(),
-               'right': pg.image.load(os.path.join(ship_folder, 'playerRight.png')).convert(),
-               'damaged': pg.image.load(os.path.join(ship_folder, 'playerDamaged.png')).convert(), }
+player_default = pg.image.load(os.path.join(ship_folder, 'player.png')).convert()
+player_default.set_colorkey(BLACK)
+player_left = pg.image.load(os.path.join(ship_folder, 'playerLeft.png')).convert()
+player_left.set_colorkey(BLACK)
+player_right = pg.image.load(os.path.join(ship_folder, 'playerRight.png')).convert()
+player_right.set_colorkey(BLACK)
+player_damaged = pg.image.load(os.path.join(ship_folder, 'playerDamaged.png')).convert()
+player_damaged.set_colorkey(BLACK)
+player_imgs = {'default': player_default,
+               'left': player_left,
+               'right': player_right,
+               'damaged': player_damaged, }
+
 # chargement des images des meteores
 meteor_imgs = {'lg': [],
                'md': [],
@@ -221,15 +306,17 @@ laser_img_list = [pg.image.load(os.path.join(laser_folder, 'laserGreen.png')).co
                   pg.image.load(os.path.join(laser_folder, 'laserRed.png')).convert(),
                   pg.image.load(os.path.join(laser_folder, 'laserBlue07.png')).convert(), ]
 # chargemenr des images des nombres
-numeral_list = []
+numeral_img = []
 for i in range(10):
     filename = 'numeral{}.png'.format(i)
     img = pg.image.load(os.path.join(numeral_folder, filename)).convert()
     img.set_colorkey(BLACK)
-    numeral_list.append(img)
+    numeral_img.append(img)
+img = pg.image.load(os.path.join(numeral_folder, 'numeralX.png')).convert()
+img.set_colorkey(BLACK)
+numeral_img.append(img)
 # Chargement des images des animations
 explosion_anim = {'lg': [], 'sm': []}
-
 for i in range(9):
     filename = 'regularExplosion0{}.png'.format(i)
     img = pg.image.load(os.path.join(explosion_folder, filename)).convert()
@@ -239,24 +326,56 @@ for i in range(9):
     img_sm = pg.transform.scale(img, (32, 32))
     explosion_anim['sm'].append(img_sm)
 
+power_imgs = {}
+gun_img = pg.image.load(os.path.join(powerup_folder, 'gun.png')).convert()
+gun_img.set_colorkey(BLACK)
+power_imgs['gun'] = gun_img
+
+cursor_img = pg.image.load(os.path.join(ship_folder, 'cursor.png')).convert_alpha()
+
+effect_imgs = {'booster': []}
+for i in range(14, 16):
+    booster_img = pg.image.load(os.path.join(effect_folder, 'fire{}.png'.format(i)))
+    booster_img.set_colorkey(BLACK)
+    effect_imgs['booster'].append(booster_img)
+
 
 all_sprites = pg.sprite.Group()
 mobs = pg.sprite.Group()
 lasers = pg.sprite.Group()
 player = Player()
-player2 = Player()
 all_sprites.add(player)
-for i in range(5):
-    new_meteor()
+power_sprites = pg.sprite.Group()
 
 # Game loop
 running = True
+game_over = True
+
 while running:
+    pg.mouse.set_visible(False)
+    if game_over:
+        show_gameover()
+        game_over = False
+        all_sprites = pg.sprite.Group()
+        # Création du score
+        score_sprites = []
+        for i in range(9):
+            score_sprite = ScoreNumeral(0, 20 * i + 20, 30)
+            score_sprites.append(score_sprite)
+            all_sprites.add(score_sprite)
+        mobs = pg.sprite.Group()
+        lasers = pg.sprite.Group()
+        player = Player()
+        all_sprites.add(player)
+        for i in range(5):
+            new_meteor()
+        score = 0
+
     clock.tick(FPS)
     # Events
     for event in pg.event.get():
         # Fermeture de la fenetre
-        if event.type == pg.QUIT:
+        if event.type == pg.QUIT or event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
             running = False
 
     # Update
@@ -266,27 +385,27 @@ while running:
     # Destruction des lasers et mobs qui se touchent
     hits = pg.sprite.groupcollide(mobs, lasers, True, True)
     for hit in hits:
-        score += 56 - hit.radius
+        score += 20 if hit.size == 'sm' else 10 if hit.size == 'md' else 1
         if hit.size == 'lg':
-            meteor_size = 'md'
-            mob1 = Meteor(meteor_size)
-            mob1.rect.x = hit.rect.centerx - 5
-            mob1.rect.y = hit.rect.centery
-            mob1.speedx = -4
-            mob1.speedy = 4
-            mobs.add(mob1)
-            mob2 = Meteor(meteor_size)
-            mob2.rect.x = hit.rect.centerx + 5
-            mob2.rect.y = hit.rect.centery
-            mob2.speedx = 4
-            mob2.speedy = 4
-            mobs.add(mob2)
-            all_sprites.add(mob1)
-            all_sprites.add(mob2)
+            # Creation du meteore de gauche quand un meteore de taille 'lg' est detruit
+            new_meteor('md', (hit.rect.centerx - 5, hit.rect.centery), (-4, 4))
+            # Creation du meteore de droite quand un meteore de taille 'lg' est detruit
+            new_meteor('md', (hit.rect.centerx + 5, hit.rect.centery), (4, 4))
+            if random.random() >= 0.9:
+                powup = Pow(hit.rect.center)
+                all_sprites.add(powup)
+                power_sprites.add(powup)
         else:
             new_meteor()
         expl = Explosion(hit.rect.center, 'lg')
         all_sprites.add(expl)
+
+    # On recupere la liste des powers up qui touchent le joueur
+    hits = pg.sprite.spritecollide(player, power_sprites, False, pg.sprite.collide_circle)
+    for hit in hits:
+        hit.kill()
+        player.power_up()
+
     # On recupere la liste des mobs qui touchent le joueur
     hits = pg.sprite.spritecollide(player, mobs, False, pg.sprite.collide_circle)
     for hit in hits:
@@ -296,14 +415,14 @@ while running:
         mob = Meteor()
         mobs.add(mob)
         all_sprites.add(mob)
-        #running = False
+        game_over = True
 
 
     # Render
-    screen.fill(BLACK)
-    screen.blit(background, background_rect)
+    screen.blit(background_img, background_rect)
     all_sprites.draw(screen)
-    draw_text(screen, str(score), 28, WIDTH / 2, 10)
+    # draw_text(screen, str(score), 28, WIDTH / 2, 10)
+    draw_score(score)
     pg.display.flip()
 
 pg.quit()
